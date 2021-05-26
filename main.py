322ecf1c7cc78ce02cdfc0cc3938cbee35a88f79
Luke <lukelovekin@gmail.com>
2021-05-26 20:33:29 +1000

    add function

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, Response
-# from camera import VideoCamera
+from camera import VideoCamera
 
 
-# pi_camera = VideoCamera(flip=False)
+pi_camera = VideoCamera(flip=True)
 
 app = Flask(__name__)
 
@@ -12,17 +12,17 @@ def index():
     return render_template('index.html')
 
 
-# def gen(camera):
-#     while True:
-#         frame = camera.get_frame()
-#         yield (b'--frame\r\n'
-#                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
-# @app.route('/video_feed')
-# def video_feed():
-#     return Response(gen(pi_camera),
-#                     mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(pi_camera),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
